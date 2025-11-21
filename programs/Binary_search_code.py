975a5adcd08fc06d73667a7af74e58c6030a0c5c
Sai Leela <mailtosaileela145@gmail.com>
2025-11-22 04:45:21 +0530

    Replaced corrupted binary search file with clean version

--- programs/Binary_search_code.py
+++ programs/Binary_search_code.py
@@ -1,47 +1,27 @@
-<<<<<<< HEAD
-def binarysearch(arr,x):
-    low=0
-    high=len(arr)-1
-    while low<=high:
-        mid=(low+high)//2
-        if arr[mid]==x:
+def binary_search(arr, x):
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        if arr[mid] == x:
             return mid
-        elif arr[mid]<x:
-            low=mid+1
+        elif arr[mid] < x:
+            low = mid + 1
         else:
-            high=mid-1
-    return -1       
-if __name__=='__main__':
-    arr=[2,3,45,7,87,43,5]
-    x=43
-    result=binarysearch(arr,x)
-    if result!=-1:
-        print("elements present in the index",result)
+            high = mid - 1
+
+    return -1
+
+
+if __name__ == '__main__':
+    arr = [2, 3, 5, 7, 43, 45, 87]   # sorted list
+    x = 43
+
+    result = binary_search(arr, x)
+
+    if result != -1:
+        print("Element present at index", result)
     else:
-        print("element is not present")
- 
-    
-=======
-def binarysearch(arr,x):
-    low=0
-    high=len(arr)-1
-    while low<=high:
-        mid=(low+high)//2
-        if arr[mid]==x:
-            return mid
-        elif arr[mid]<x:
-            low=mid+1
-        else:
-            high=mid-1
-    return -1       
-if __name__=='__main__':
-    arr=[2,3,45,7,87,43,5]
-    x=43
-    result=binarysearch(arr,x)
-    if result!=-1:
-        print("elements present in the index",result)
-    else:
-        print("element is not present")
- 
-    
->>>>>>> 43d0729283787bdad5828806f7eb7bed97c4c756
+        print("Element not present")
