5faf5473c728f09bbcbe7144737374b434d001c4
Saileela <156500159+Saileela145@users.noreply.github.com>
2025-11-30 19:46:21 +0530

    Refactor insertion_sort for clarity
    Refactor insertion_sort to improve readability by storing the length of the array in a variable.

--- programs/day3/insertion_sort.py
+++ programs/day3/insertion_sort.py
@@ -1,10 +1,13 @@
 def insertion_sort(arr):
     nums = arr.copy()
-    for i in range(1, len(nums)):
+    n = len(nums)
+
+    for i in range(1, n):
         key = nums[i]
         j = i - 1
         while j >= 0 and nums[j] > key:
-            nums[j+1] = nums[j]
+            nums[j + 1] = nums[j]
             j -= 1
-        nums[j+1] = key
+        nums[j + 1] = key
+
     return nums
