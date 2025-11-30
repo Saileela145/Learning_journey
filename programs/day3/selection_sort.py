3ba67ff5a53ab7c4b48176139e54477cd0297431
Saileela <156500159+Saileela145@users.noreply.github.com>
2025-11-30 19:58:45 +0530

    Update selection_sort.py

--- programs/day3/selection_sort.py
+++ programs/day3/selection_sort.py
@@ -1,9 +1,13 @@
-def selection(arr,size):
-    for i in range(size):
-        minvalue=i
-        for j in range(i+1,size):
-            if arr[j]<arr[minvalue]:
-               minvalue=j
-        arr[i],arr[minvalue]=arr[minvalue],arr[i]
-    return arr
+def selection_sort(arr):
+    nums = arr.copy()
+    n = len(nums)
+    
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            if nums[j] < nums[min_index]:
+                min_index = j
 
+        nums[i], nums[min_index] = nums[min_index], nums[i]
+
+    return nums
