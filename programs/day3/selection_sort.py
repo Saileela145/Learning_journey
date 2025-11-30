e4effb388ab60f1c33fe2e48243d5447735d03b5
Saileela <156500159+Saileela145@users.noreply.github.com>
2025-11-30 17:41:26 +0530

    Update selection_sort.py

--- programs/day3/selection_sort.py
+++ programs/day3/selection_sort.py
@@ -6,7 +6,4 @@ def selection(arr,size):
                minvalue=j
         arr[i],arr[minvalue]=arr[minvalue],arr[i]
     return arr
-mylist=[65,2,21,7,3,98,22,87,-45,-3,-9]
-size=len(mylist)
-result=selection(mylist,size)
-print(result)
+
