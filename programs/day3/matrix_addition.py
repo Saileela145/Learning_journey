70fe8d146944819ca130f388e01ceca3af688b06
Saileela <156500159+Saileela145@users.noreply.github.com>
2025-11-30 19:50:23 +0530

    Update matrix_addition.py

--- programs/day3/matrix_addition.py
+++ programs/day3/matrix_addition.py
@@ -1,11 +1,13 @@
-def addmatrix(matrix1,matrix2):
-    if len(matrix1)!=len(matrix2) or len(matrix1[0])!=len(matrix2[0]):
+def matrix_addition(matrix1, matrix2):
+    if len(matrix1) != len(matrix2) or len(matrix1[0]) != len(matrix2[0]):
         raise ValueError("the matrix must have same dimensions to perform addition")
-    rows=len(matrix1)
-    cols=len(matrix1[0])
-    result =[[0 for _ in range(cols)]for _ in range(rows)]
+
+    rows = len(matrix1)
+    cols = len(matrix1[0])
+    result = [[0 for _ in range(cols)] for _ in range(rows)]
+
     for i in range(rows):
         for j in range(cols):
-            result[i][j]=matrix1[i][j]+matrix2[i][j]
-    return result
+            result[i][j] = matrix1[i][j] + matrix2[i][j]
 
+    return result
