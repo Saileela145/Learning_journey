8f9b35b6bc9b7fbd2f701d90cbc88601ccbc3499
Saileela <156500159+Saileela145@users.noreply.github.com>
2025-11-30 17:57:46 +0530

    Update bubble_sort.py

--- programs/day3/bubble_sort.py
+++ programs/day3/bubble_sort.py
@@ -1,12 +1,11 @@
-def bubble(arr):
-    n=len(arr)
-    for i in range(n-1):
-        swapped=False
-        for j in range(0,n-i-1):
-            if arr[j]>arr[j+1]:
-                arr[j],arr[j+1]=arr[j+1],arr[j]
-                swapped=True
-        if swapped==False:
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n - 1):
+        swapped = False
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+        if swapped == False:
             break
     return arr
-
