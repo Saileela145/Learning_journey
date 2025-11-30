66f3da2966ffa1f429bfc8ca864cae8a1c00538b
Saileela <156500159+Saileela145@users.noreply.github.com>
2025-11-30 17:38:28 +0530

    Remove example usage from linear_search.py
    Removed example usage of linear search from the script.

--- programs/day3/linear_search.py
+++ programs/day3/linear_search.py
@@ -4,19 +4,3 @@ def linear(arr,target):
         if arr[i]==target:
             return i
     return -1
-mylist=[6,32,76,9,2,87,3,65]
-target=76
-result=linear(mylist,target)
-if result!=-1:
-    print(f"the {target} value is in index{result}")
-else:
-    print("target not found")
-
-mylist1=[45,65,9,3,0,67,32,98]
-mylist1.sort()
-targetvalue=67
-result1=linear(mylist1,targetvalue)
-if result1!=-1:
-   print(f"the {targetvalue} value is in index{result1}")
-else:
-    print("target not found")
