055068a47bb71857758d98064dd6c14a75a510ca
Saileela <156500159+Saileela145@users.noreply.github.com>
2025-11-30 19:29:02 +0530

    Update binary_search.py

--- programs/day3/binary_search.py
+++ programs/day3/binary_search.py
@@ -1,13 +1,16 @@
-def binary(arr,target):
-    low=0
-    high=len(arr)-1
-    while low<=high:
-        mid=(low+high)//2
-        mvalue=arr[mid]
-        if mvalue==target:
+def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        mvalue = arr[mid]
+
+        if mvalue == target:
             return mid
-        elif target<mvalue:
-            high=mid-1
+        elif target < mvalue:
+            high = mid - 1
         else:
-            low=mid+1
+            low = mid + 1
+
     return -1
