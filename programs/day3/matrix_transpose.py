160f648717df95741ea51ce35edfb61d94683636
Saileela <156500159+Saileela145@users.noreply.github.com>
2025-11-30 19:52:28 +0530

    Create matrix_transpose function
    Refactor matrix transpose logic into a function.

--- programs/day3/matrix_transpose.py
+++ programs/day3/matrix_transpose.py
@@ -1,8 +1,10 @@
-matrix=[[1,2,3],[3,4,5],[8,6,9]]
-rows=len(matrix)
-cols=len(matrix[0])
-result=[[0 for _ in range(rows)]for _ in range(cols)]
-for i in range(rows):
-    for j in range(cols):
-        result[j][i]=matrix[i][j]
-        
+def matrix_transpose(matrix):
+    rows = len(matrix)
+    cols = len(matrix[0])
+    result = [[0 for _ in range(rows)] for _ in range(cols)]
+
+    for i in range(rows):
+        for j in range(cols):
+            result[j][i] = matrix[i][j]
+
+    return result
